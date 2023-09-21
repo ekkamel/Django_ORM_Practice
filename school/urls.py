00022cba976af9431a1304b3a6c302f70516cea7
Emad Kamel <ekkamel@gmail.com>
2023-09-22 01:31:41 +0300

    added student search by id

--- school/urls.py
+++ school/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from school.models import School, Student
-from .views import get_schools, get_students
+from .views import get_schools, get_students, get_student
 
 urlpatterns = [
     path("students/", get_students, name="get_students"),
     path("schools/", get_schools, name="get_schools"),
+    path("student/<int:student_id>", get_student, name="get_student"), 
 ]
--- school/views.py
+++ school/views.py
@@ -7,19 +7,36 @@ from django.shortcuts import render
 def get_students(request): 
     students = Student.objects.all()
     
-    student_data = []
+    students_data = []
     for student in students: 
-        student_data.append({
+        students_data.append({
+            "id": student.id,
             "name": student.name, 
             "grade": student.grade,  
             "school": student.school.name,            
         })
 
     data = {
-        "students": student_data, 
+        "students": students_data, 
     }
 
     return JsonResponse(data, safe=False)
+
+
+
+def get_student(request, student_id): 
+    student = Student.objects.get(pk=student_id)
+    
+    student_data = {
+        "id": student.id,
+        "name": student.name, 
+        "grade": student.grade,  
+        "school": student.school.name, 
+        "registered": student.reg_date,
+    }
+    
+    return JsonResponse(student_data)    
+    
 
 
 def get_schools(request): 
