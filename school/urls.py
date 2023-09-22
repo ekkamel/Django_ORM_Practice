a31a669937d343526681192a1708e1c9ec693151
Emad Kamel <ekkamel@gmail.com>
2023-09-22 22:16:05 +0300

    added views

--- school/urls.py
+++ school/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from school.models import School, Student
-from .views import get_schools, get_students, get_student
+from .views import get_schools, get_students, get_student, get_filter_options, student_grade_chart
 
 urlpatterns = [
     path("students/", get_students, name="get_students"),
     path("schools/", get_schools, name="get_schools"),
     path("student/<int:student_id>", get_student, name="get_student"), 
+    path("grade-chart/<int:year>/", student_grade_chart, name="student-grade-chart"),
+    path("filter-options/", get_filter_options, name="chart-filter-options"), 
 ]
--- school/views.py
+++ school/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from school.models import School, Student
 from django.shortcuts import render
+from django.db.models.functions import ExtractYear, ExtractMonth
+from django.db.models import Count, F, Sum, Avg
+
+from .utils.charts import months, colorPrimary, colorSuccess, colorDanger, generate_color_palette, get_year_dict
 
 # Create your views here.
 def get_students(request): 
@@ -13,7 +17,8 @@ def get_students(request):
             "id": student.id,
             "name": student.name, 
             "grade": student.grade,  
-            "school": student.school.name,            
+            "school": student.school.name,  
+            "School_location": student.school.location          
         })
 
     data = {
@@ -54,3 +59,40 @@ def get_schools(request):
     }
     
     return JsonResponse(data, safe=False)
+
+
+# This view populates the Options drop down list
+def get_filter_options(request): 
+    grouped_registrations = Student.objects.annotate(year=ExtractYear("reg_date")).values("year").order_by("-year").distinct()
+    options = [registration["year"] for registration in grouped_registrations]
+    
+    return JsonResponse({
+        "options": options,
+    })
+    
+
+def student_grade_chart(request, year): 
+    grades = Student.objects.filter(reg_date__year=year)
+    grouped_grades = grades.values("grade").annotate(count=Count("id"))\
+        .values("grade", "count").order_by("grade") 
+    
+    grade_dict = dict()
+    
+    #for grade in Student.grade:
+    #    grade_dict[grade[1]] = 0 
+    
+    for group in grouped_grades: 
+        grade_dict[dict(Student.grade)[group["grade"]]] = group["count"]
+        
+    return JsonResponse({
+        "title": f"Registered grades in {year}", 
+        "data": {
+            "labels": list(grade_dict.keys()), 
+            "datasets": [{
+                "label": "Students", 
+                "backgroundColor": generate_color_palette(len(grade_dict)), 
+                "borderColor": generate_color_palette(len(grade_dict)), 
+                "data": list(grade_dict.values()), 
+            }]
+        },
+    })
